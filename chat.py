538b03c8ad8e7e49dc165778d0f6c149d7674bbc
Davinson13 <dadiaz1h@gmail.com>
2025-03-04 23:25:56 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -104,4 +104,6 @@ def chatbot():
 
 # 🔹 Ejecutar la aplicación en modo local
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
